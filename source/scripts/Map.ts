70727e6e37990be36fc80b4c3dd297a9bc4b17b3
Graham <graham.gibbons@gmail.com>
2018-02-26 16:44:37 -0800

    Refactor

--- source/scripts/Map.ts
+++ source/scripts/Map.ts
@@ -4,61 +4,9 @@ import Component from "wedges/lib/Component";
 import { airspaceService } from ".";
 
 export default class Map extends Component {
-    private shape: [number, number][] = [];
 
     render(element: Element) {
-        const map = new mapbox.Map({
-            container: element,
-            style: "mapbox://styles/mapbox/light-v9",
-            center: [-122.6765, 45.5231],
-            zoom: 10,
-            pitch: 45
-        });
-
-        const updateAirspaces = async () => {
-            const bounds = boundsWithPitch(map);
-
-            const airspaces = await airspaceService.airspaces(
-                bounds.getWest(),
-                bounds.getEast(),
-                bounds.getSouth(),
-                bounds.getNorth());
-
-            const source = map.getSource("airspaces") as mapbox.GeoJSONSource;
-
-            source.setData(airspaces);
-        };
-
-        const updateShape = () => {
-            const source = map.getSource("shape") as mapbox.GeoJSONSource;
-            let shape = this.shape.slice();
-            if (shape.length > 0)
-                shape = shape.concat([this.shape[0]]);
-            console.log(shape);
-            source.setData({
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [shape]
-                }
-            });
-        }
-
-        map.on("load", () => {
-            style(map);
-            map.on("moveend", updateAirspaces);
-            map.on("click", (event: any) => {
-                const position = event.lngLat;
-                this.shape.push([position.lng, position.lat]);
-                updateShape();
-            });
-            map.on("contextmenu", () => {
-                this.shape = [];
-                updateShape();
-            });
-            updateAirspaces();
-        });
+        const map = createMap(element);
 
         return {
             update: () => { },
@@ -68,126 +16,189 @@ export default class Map extends Component {
     }
 }
 
-function boundsWithPitch(map: mapbox.Map): mapbox.LngLatBounds {
-    // getBounds() is broken with a .pitch unfortunately
-    const canvas = map.getCanvas();
-    const rect = canvas.getBoundingClientRect();
-    return [
-        [rect.left, rect.top],
-        [rect.left, rect.bottom],
-        [rect.right, rect.bottom],
-        [rect.right, rect.top]
-    ]
-        .map(_ => map.unproject(_))
-        .reduce(
-            (bounds, point) => bounds.extend(point),
-            new mapbox.LngLatBounds());
-}
+function createMap(element: Element): mapbox.Map {
 
-function style(map: mapbox.Map) {
-    map.addSource("airspaces", {
-        "type": "geojson",
-        "data": {
-            "type": "FeatureCollection",
-            "features": []
-        }
+    let shape: [number, number][] = [];
+
+    const map = new mapbox.Map({
+        container: element,
+        style: "mapbox://styles/mapbox/light-v9",
+        center: [-122.6765, 45.5231],
+        zoom: 10,
+        pitch: 45
     });
-    map.addSource("shape", {
-        "type": "geojson",
-        "data": {
+
+    map.on("load", () => {
+        style();
+        map.on("moveend", updateAirspaces);
+        map.on("click", (event: any) => {
+            const position = event.lngLat;
+            shape.push([position.lng, position.lat]);
+            updateShape();
+        });
+        map.on("contextmenu", () => {
+            shape = [];
+            updateShape();
+        });
+        updateAirspaces();
+    });
+
+    async function updateAirspaces() {
+        const bounds = boundsWithPitch();
+
+        const airspaces = await airspaceService.airspaces(
+            bounds.getWest(),
+            bounds.getEast(),
+            bounds.getSouth(),
+            bounds.getNorth());
+
+        const source = map.getSource("airspaces") as mapbox.GeoJSONSource;
+
+        source.setData(airspaces);
+    };
+
+    function updateShape() {
+        const source = map.getSource("shape") as mapbox.GeoJSONSource;
+        let vertices = shape.slice();
+        if (vertices.length > 0)
+            vertices = vertices.concat([shape[0]]);
+        source.setData({
             "type": "Feature",
             "properties": {},
             "geometry": {
                 "type": "Polygon",
-                "coordinates": [[[
-                ]]]
+                "coordinates": [vertices]
             }
-        }
-    });
+        });
+    }
 
-    const base = ["to-number", ["get", "floor"], 0];
-    const height = [
-        "-",
-        ["to-number", ["get", "ceiling"], 0],
-        base
-    ];
+    function boundsWithPitch(): mapbox.LngLatBounds {
+        // map.getBounds() is broken with a .pitch unfortunately
+        const canvas = map.getCanvas();
+        const rect = canvas.getBoundingClientRect();
+        return [
+            [rect.left, rect.top],
+            [rect.left, rect.bottom],
+            [rect.right, rect.bottom],
+            [rect.right, rect.top]
+        ]
+            .map(_ => map.unproject(_))
+            .reduce(
+                (bounds, point) => bounds.extend(point),
+                new mapbox.LngLatBounds());
+    }
 
-    map.addLayer({
-        "id": "airspaces-extrusion-laanc",
-        "source": "airspaces",
-        "type": "fill-extrusion",
-        "filter": ["==", ["get", "layer"], "YELLOW.USA.FAA_LAANC"],
-        "paint": {
-            "fill-extrusion-color": "#eeee77",
-            "fill-extrusion-height": height,
-            "fill-extrusion-base": base,
-            "fill-extrusion-opacity": 0.6
-        }
-    });
-    map.addLayer({
-        "id": "airspaces-extrusion-red",
-        "source": "airspaces",
-        "type": "fill-extrusion",
-        "filter": ["==", ["get", "layer"], "RED.USA"],
-        "paint": {
-            "fill-extrusion-color": "#ee3333",
-            "fill-extrusion-height": height,
-            "fill-extrusion-base": base,
-            "fill-extrusion-opacity": 0.6
-        }
-    });
-    map.addLayer({
-        "id": "airspaces-extrusion-other",
-        "source": "airspaces",
-        "type": "fill-extrusion",
-        "filter": ["all",
-            ["!=", ["get", "layer"], "YELLOW.USA.FAA_LAANC"],
-            ["!=", ["get", "layer"], "RED.USA"]],
-        "paint": {
-            "fill-extrusion-color": "#aaa",
-            "fill-extrusion-height": height,
-            "fill-extrusion-base": base,
-            "fill-extrusion-opacity": 0.33
-        }
-    });
-    map.addLayer({
-        "id": "airspaces-outline",
-        "source": "airspaces",
-        "type": "line",
-        "paint": {
-            "line-width": 1,
-            "line-opacity": 0.05
-        }
-    });
-    map.addLayer({
-        "id": "airspaces-labels",
-        "source": "airspaces",
-        "type": "symbol",
-        "filter": ["==", ["get", "layer"], "YELLOW.USA.FAA_LAANC"],
-        "layout": {
-            "text-field": "{ceiling}",
-            "text-size": 12,
-        }
-    });
+    function style() {
+        map.addSource("airspaces", {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": []
+            }
+        });
+        map.addSource("shape", {
+            "type": "geojson",
+            "data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [[[
+                    ]]]
+                }
+            }
+        });
 
-    map.addLayer({
-        "id": "shape-extrusion",
-        "source": "shape",
-        "type": "fill-extrusion",
-        "paint": {
-            "fill-extrusion-color": "#33ee33",
-            "fill-extrusion-height": 300,
-            "fill-extrusion-opacity": 0.8
-        }
-    });
-    map.addLayer({
-        "id": "shape-outline",
-        "source": "shape",
-        "type": "line",
-        "paint": {
-            "line-width": 5,
-            "line-opacity": 0.25,
-            "line-color": "#33ee33"
-        }
-    });
+        const base = ["to-number", ["get", "floor"], 0];
+        const height = [
+            "-",
+            ["to-number", ["get", "ceiling"], 0],
+            base
+        ];
+
+        map.addLayer({
+            "id": "airspaces-extrusion-laanc",
+            "source": "airspaces",
+            "type": "fill-extrusion",
+            "filter": ["==", ["get", "layer"], "YELLOW.USA.FAA_LAANC"],
+            "paint": {
+                "fill-extrusion-color": "#eeee77",
+                "fill-extrusion-height": height,
+                "fill-extrusion-base": base,
+                "fill-extrusion-opacity": 0.6
+            }
+        });
+        map.addLayer({
+            "id": "airspaces-extrusion-red",
+            "source": "airspaces",
+            "type": "fill-extrusion",
+            "filter": ["==", ["get", "layer"], "RED.USA"],
+            "paint": {
+                "fill-extrusion-color": "#ee3333",
+                "fill-extrusion-height": height,
+                "fill-extrusion-base": base,
+                "fill-extrusion-opacity": 0.6
+            }
+        });
+        map.addLayer({
+            "id": "airspaces-outline",
+            "source": "airspaces",
+            "type": "line",
+            "paint": {
+                "line-width": 1,
+                "line-opacity": 0.05
+            }
+        });
+        map.addLayer({
+            "id": "airspaces-labels",
+            "source": "airspaces",
+            "type": "symbol",
+            "filter": ["==", ["get", "layer"], "YELLOW.USA.FAA_LAANC"],
+            "layout": {
+                "text-field": "{ceiling}",
+                "text-size": 12,
+            }
+        });
+
+        map.addLayer({
+            "id": "shape-extrusion",
+            "source": "shape",
+            "type": "fill-extrusion",
+            "paint": {
+                "fill-extrusion-color": "#33ee33",
+                "fill-extrusion-height": 280,
+                "fill-extrusion-opacity": 0.8
+            }
+        });
+        map.addLayer({
+            "id": "shape-outline",
+            "source": "shape",
+            "type": "line",
+            "paint": {
+                "line-width": 5,
+                "line-opacity": 0.25,
+                "line-color": "#33ee33"
+            }
+        });
+
+        map.addLayer({
+            "id": "airspaces-extrusion-other",
+            "source": "airspaces",
+            "type": "fill-extrusion",
+            "filter": ["all",
+                ["!=", ["get", "layer"], "YELLOW.USA.FAA_LAANC"],
+                ["!=", ["get", "layer"], "RED.USA"]],
+            "paint": {
+                "fill-extrusion-color": "#aaa",
+                "fill-extrusion-height": height,
+                "fill-extrusion-base": base,
+                "fill-extrusion-opacity": 0.33
+            }
+        });
+    }
+
+    return map;
 }
+
+
+
